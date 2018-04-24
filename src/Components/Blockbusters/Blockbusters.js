02facd6cdcb02940e8a4bab8089a9a5439766e3f
Michael Dance <michael.dance@gmail.com>
2018-04-24 13:53:55 -0600

    Added axios.get error handling

--- src/Components/Blockbusters/Blockbusters.js
+++ src/Components/Blockbusters/Blockbusters.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import React, { Component } from 'react';
 
-const apikey = '7740f124758295f3deea367c18f010e4'; //This is api key for movie database
+const apiKey = '7740f124758295f3deea367c18f010e4'; //This is api key for movie database
 const apiURL = 'https://api.themoviedb.org/3/movie/';
 
 
@@ -12,38 +12,55 @@ const apiURL = 'https://api.themoviedb.org/3/movie/';
 // title and year from OMDB
 
 export default class Blockbusters extends Component {
-    constructor(){
+    constructor() {
         super();
 
         this.state = {
             blockbusters: []
         };
 
-        this.updateList =this.updateList.bind( this );
+        this.updateList = this.updateList.bind( this );
+    }
 
-        }
-
-    updateList(){
-        
+    updateList() {
         let newRandomList = [];
-        for (let i = 0; i<9;i++){
-        let randomMovie = Math.floor(Math.random()*100000);
-        axios.get(`${apiURL}${randomMovie}?api_key=${apikey}`).then(results=>{
-            if(results.status_code===34){
-                i--;
-            } else {
-            newRandomList.push(results.data.title)
-            console.log(randomMovie);
-            console.log(results.data.title)
+
+        for (let i = 0; i<9; i++) {
+            let randomMovie = Math.floor(Math.random()*100000);
+            let url = `${apiURL}${randomMovie}?api_key=${apiKey}`
+    
+            axios.get(url)
+            .then(
+                results => {
+                    if (results.status_code===34) {
+                        i--;
+                    } else {
+                        let title = results.data.title;
+
+                        newRandomList.push(title)
+            
+                        console.log(randomMovie);
+                        console.log(title)
+                    }
+                }
+            ).catch(
+                error => {
+                    console.log(error)
+                }
+            )
         }
-        })
-     }
-        this.setState({blockbusters:newRandomList});       
+        
+        this.setState(
+            {
+                blockbusters:newRandomList
+            }
+        );       
     }
 
     render() {
-        const { blockbusters} =this.state;
-        return(
+        const {blockbusters} = this.state;
+        
+        return (
             <div>
                 <p>{blockbusters}</p>
                 <button onClick={this.updateList}>Update List</button>
